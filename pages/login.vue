2eca1ccb42d1d3a4b1e7080f8706801984de0c5f
Matan Shaviro <matan3sh@gmail.com>
2020-01-06 19:56:34 +0200

    Adds Vuelidate package

--- pages/login.vue
+++ pages/login.vue
@@ -7,46 +7,50 @@
           <p class="subtitle has-text-grey">Please login to proceed.</p>
           <div class="box">
             <figure class="avatar">
-              <img src="https://via.placeholder.com/300">
+              <img src="https://via.placeholder.com/300" />
             </figure>
             <form>
               <div class="field">
                 <div class="control">
-                  <input class="input is-large"
-                         type="email"
-                         placeholder="Your Email"
-                         autofocus=""
-                         autocomplete="email">
+                  <input
+                    v-model="form.email"
+                    class="input is-large"
+                    type="email"
+                    placeholder="Your Email"
+                    autofocus
+                    autocomplete="email"
+                  />
                   <!-- <div class="form-error">
                     <span class="help is-danger">Email is required</span>
                     <span class="help is-danger">Email address is not valid</span>
-                  </div> -->
+                  </div>-->
                 </div>
               </div>
               <div class="field">
                 <div class="control">
                   <input
+                    v-model="form.password"
                     class="input is-large"
                     type="password"
                     placeholder="Your Password"
-                    autocomplete="current-password">
+                    autocomplete="current-password"
+                  />
                   <!-- <div class="form-error">
                     <span class="help is-danger">Password is required</span>
-                  </div> -->
+                  </div>-->
                 </div>
               </div>
               <!-- Login Button -->
               <button
-                @click.prevent="() => {}"
-                class="button is-block is-info is-large is-fullwidth">
-                Login
-              </button>
+                @click.prevent="login"
+                class="button is-block is-info is-large is-fullwidth"
+              >Login</button>
             </form>
           </div>
           <p class="has-text-grey">
             <a>Sign In With Google</a> &nbsp;·&nbsp;
-            <nuxt-link to="/register">Sign Up</nuxt-link> &nbsp;·&nbsp;
-
+            <nuxt-link to="/register">Sign Up</nuxt-link>
+&nbsp;·&nbsp;
             <a href="../">Need Help?</a>
           </p>
         </div>
@@ -55,31 +59,63 @@
   </section>
 </template>
 
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+export default {
+  data() {
+    return {
+      form: {
+        email: null,
+        password: null
+      }
+    };
+  },
+  validation: {
+    form: {
+      email: {
+        email,
+        required
+      },
+      password: {
+        required
+      }
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.form)
+    }
+  }
+};
+</script>
+
 <style scoped>
-  .hero.is-success {
-    background: #F2F6FA;
-  }
-  .hero .nav, .hero.is-success .nav {
-    -webkit-box-shadow: none;
-    box-shadow: none;
-  }
-  .box {
-    margin-top: 5rem;
-  }
-  .avatar {
-    margin-top: -70px;
-    padding-bottom: 20px;
-  }
-  .avatar img {
-    height: 128px;
-    width: 128px;
-    padding: 5px;
-    background: #fff;
-    border-radius: 50%;
-    -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
-    box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
-  }
-  p.subtitle {
-    padding-top: 1rem;
-  }
+.hero.is-success {
+  background: #f2f6fa;
+}
+.hero .nav,
+.hero.is-success .nav {
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+.box {
+  margin-top: 5rem;
+}
+.avatar {
+  margin-top: -70px;
+  padding-bottom: 20px;
+}
+.avatar img {
+  height: 128px;
+  width: 128px;
+  padding: 5px;
+  background: #fff;
+  border-radius: 50%;
+  -webkit-box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1),
+    0 0 0 1px rgba(10, 10, 10, 0.1);
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+}
+p.subtitle {
+  padding-top: 1rem;
+}
 </style>
