f21cec9c0d50c0396b7bc8acb46cfc151f951300
Matan Shaviro <matan3sh@gmail.com>
2020-01-06 20:13:05 +0200

    Display validation errors in Login form

--- pages/login.vue
+++ pages/login.vue
@@ -7,7 +7,7 @@
           <p class="subtitle has-text-grey">Please login to proceed.</p>
           <div class="box">
             <figure class="avatar">
-              <img src="https://via.placeholder.com/300" />
+              <img src="https://via.placeholder.com/300">
             </figure>
             <form>
               <div class="field">
@@ -17,13 +17,12 @@
                     class="input is-large"
                     type="email"
                     placeholder="Your Email"
-                    autofocus
-                    autocomplete="email"
-                  />
-                  <!-- <div class="form-error">
-                    <span class="help is-danger">Email is required</span>
-                    <span class="help is-danger">Email address is not valid</span>
-                  </div>-->
+                    autofocus=""
+                    autocomplete="email">
+                  <div v-if="$v.form.email.$error" class="form-error">
+                    <span v-if="!$v.form.email.required" class="help is-danger">Email is required</span>
+                    <span v-if="!$v.form.email.emailValidator" class="help is-danger">Email address is not valid</span>
+                  </div>
                 </div>
               </div>
               <div class="field">
@@ -33,24 +32,24 @@
                     class="input is-large"
                     type="password"
                     placeholder="Your Password"
-                    autocomplete="current-password"
-                  />
-                  <!-- <div class="form-error">
-                    <span class="help is-danger">Password is required</span>
-                  </div>-->
+                    autocomplete="current-password">
+                  <div v-if="$v.form.password.$error" class="form-error">
+                    <span v-if="!$v.form.password.required" class="help is-danger">Password is required</span>
+                  </div>
                 </div>
               </div>
               <!-- Login Button -->
               <button
                 @click.prevent="login"
-                class="button is-block is-info is-large is-fullwidth"
-              >Login</button>
+                class="button is-block is-info is-large is-fullwidth">
+                Login
+              </button>
             </form>
           </div>
           <p class="has-text-grey">
             <a>Sign In With Google</a> &nbsp;·&nbsp;
-            <nuxt-link to="/register">Sign Up</nuxt-link>
-&nbsp;·&nbsp;
+            <nuxt-link to="/register">Sign Up</nuxt-link> &nbsp;·&nbsp;
+
             <a href="../">Need Help?</a>
           </p>
         </div>
@@ -58,7 +57,6 @@
     </div>
   </section>
 </template>
-
 <script>
 import { required, email } from 'vuelidate/lib/validators'
 export default {
@@ -68,12 +66,12 @@ export default {
         email: null,
         password: null
       }
-    };
+    }
   },
-  validation: {
+  validations: {
     form: {
       email: {
-        email,
+        emailValidator: email,
         required
       },
       password: {
@@ -83,39 +81,37 @@ export default {
   },
   methods: {
     login() {
-      console.log(this.form)
+      this.$v.form.$touch()
     }
   }
-};
+}
 </script>
 
 <style scoped>
-.hero.is-success {
-  background: #f2f6fa;
-}
-.hero .nav,
-.hero.is-success .nav {
-  -webkit-box-shadow: none;
-  box-shadow: none;
-}
-.box {
-  margin-top: 5rem;
-}
-.avatar {
-  margin-top: -70px;
-  padding-bottom: 20px;
-}
-.avatar img {
-  height: 128px;
-  width: 128px;
-  padding: 5px;
-  background: #fff;
-  border-radius: 50%;
-  -webkit-box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1),
-    0 0 0 1px rgba(10, 10, 10, 0.1);
-  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
-}
-p.subtitle {
-  padding-top: 1rem;
-}
+  .hero.is-success {
+    background: #F2F6FA;
+  }
+  .hero .nav, .hero.is-success .nav {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
+  .box {
+    margin-top: 5rem;
+  }
+  .avatar {
+    margin-top: -70px;
+    padding-bottom: 20px;
+  }
+  .avatar img {
+    height: 128px;
+    width: 128px;
+    padding: 5px;
+    background: #fff;
+    border-radius: 50%;
+    -webkit-box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+    box-shadow: 0 2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1);
+  }
+  p.subtitle {
+    padding-top: 1rem;
+  }
 </style>
