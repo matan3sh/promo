f96aa868c195cfe37f87fb333cb431cee593e99d
Matan Shaviro <matan3sh@gmail.com>
2020-01-09 17:41:09 +0200

    Preserve auth state

--- store/auth.js
+++ store/auth.js
@@ -1,35 +1,49 @@
-
 export const state = () => ({
     user: null
-})
-
-export const getters = {
+  })
+  
+  export const getters = {
     authUser(state) {
-        return state.user || null
+      return state.user || null
     },
     isAuthenticated(state) {
-        return !!state.user
+      return !!state.user
     },
     isAdmin(state) {
-        return state.user &&
-               state.user.role && 
-               state.user.role === 'admin'
+      return state.user &&
+             state.user.role &&
+             state.user.role === 'admin'
     }
-}
-
-export const actions = {
+  }
+  
+  export const actions = {
     login({commit, state}, loginData) {
-        return this.$axios.$post('/api/v1/users/login', loginData)
-            .then(user => {
-                commit('setAuthUser', user)
-                return state.user
-            })
-            .catch((error) => Promise.reject(error))
+      return this.$axios.$post('/api/v1/users/login', loginData)
+        .then(user => {
+          commit('setAuthUser', user)
+          return state.user
+        })
+        .catch(error => Promise.reject(error))
+    },
+    getAuthUser({commit, getters, state}) {
+      const authUser = getters.authUser
+  
+      if (authUser) { return Promise.resolve(authUser) }
+  
+      return this.$axios.$get('/api/v1/users/me')
+        .then((user) => {
+          commit('setAuthUser', user)
+          return state.user
+        })
+        .catch((error) => {
+          commit('setAuthUser', null)
+          return Promise.reject(error)
+        })
     }
-}
-
-export const mutations = {
+  }
+  
+  export const mutations = {
     setAuthUser(state, user) {
-        return state.user = user
+      state.user = user
     }
-}
+  }
