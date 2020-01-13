51c01a0f57970671064139c7cf47b583e8e09522
Matan Shaviro <matan3sh@gmail.com>
2020-01-13 11:05:57 +0200

    Adds not authorized pages

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,7 +2,6 @@ export default function({store, redirect}) {
     const isAdmin = store.getters['auth/isAdmin']
 
     if (!isAdmin) {
-        // navigate later to notAuthenticated page
-        return redirect('/')
+        return redirect('/notAuthorized')
     }
 }
